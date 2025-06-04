5d63b8ed5e2c3bf6d1348f2bc65294e2cc1e5fc8
Leif <leifweikamp@uni-rostock.de>
2025-06-04 13:14:41 +0200

    reduced iterations when constructing c[j,i], elaborated comments, fixed typos

--- src/GSorthogonalization.jl
+++ src/GSorthogonalization.jl
@@ -5,33 +5,39 @@ function GSorthogonalization_expan(e, a)
     # s is the magnitude of vector components in a such that the component of e that is in span(a) is sum(si0*ai)
 
     T = promote_type(eltype(e), eltype(a[1]))
-    #Orthogonalisation. u is a set of orthogonal vectors such that span(u) = span(a)
+
+    ## Orthogonalization using the Gram-Schmidt procedure.
+    # We want to find a set of orthogonal vectors u such that span(u) = span(a)
+    # Since vectors a don't have to be linearly independent some u may be 0.
     n = length(a)
     u = Vector{Vector{Float64}}(undef, n)
-    u[1] = a[1]
-    c = zeros(T, n, n)
+
+    u[1] = a[1] # start of process
+    
+    c = zeros(T, n, n) # coefficients of u[k] to get a[i]
     for i in 2:n
         sum_term = zeros(T, 3) #assumed 3D space, so vector u has 3 elements
         for k in 1:(i - 1)
             ukn = norm(u[k])
-            if ukn^2 < zerotol
+            if ukn^2 < zerotol # special treatment of linearly dependent vectors a_j
                 c[k, i] = T(0.0)
             else
-                c[k, i] = dot(u[k], a[i]) / (ukn^2)
+                c[k, i] = dot(u[k], a[i]) / ukn^2
             end
             sum_term += c[k, i] * u[k]
         end
         u[i] = a[i] - sum_term
     end
 
-    #Find all projection coefficients c
-    for i in 1:n
-        for j in 1:n
+    #Find all other projection coefficients c
+    for j in 1:n
+        for i in 1:j
             ujn = norm(u[j])
             if ujn^2 < zerotol
-                ujn = T(1)
+                c[j, i] = T(0.0)
+            else
+                c[j, i] = dot(u[j], a[i]) / ujn^2
             end
-            c[j, i] = dot(u[j], a[i]) / (ujn^2)
         end
     end
 
@@ -41,21 +47,21 @@ function GSorthogonalization_expan(e, a)
     for j in n:-1:1
         b[j] = dot(u[j], e)
         sum_term = T(0.0)
-        Ajj = c[j, j] * ((norm(u[j]))^2)
+        Ajj = c[j, j] * norm(u[j])^2
         if Ajj < zerotol
             s[j] = T(0.0)
-        elseif j == n #just for the first iteration because there is no other terms
+        elseif j == n #sum_term is 0, so ignore it
             s[j] = b[j] / Ajj
         else
-            for i in (j + 1):n
-                Aji = c[j, i] * ((norm(u[j]))^2)
+            for i in (j + 1):n #calculate sum_term
+                Aji = c[j, i] * norm(u[j])^2
                 sum_term += Aji * s[i]
             end
             s[j] = (b[j] - sum_term) / Ajj
         end
     end
 
-    #find h
+    #Find h
     sum_term = zeros(T, 3) #same as above, 3D space so vector u has 3 elements
     for i in 1:n
         sum_term += s[i] * a[i]
