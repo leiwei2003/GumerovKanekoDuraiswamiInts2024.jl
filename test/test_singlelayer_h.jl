b049211bed82e37c8557473f2d96eb8a6479991b
Leif <leifweikamp@uni-rostock.de>
2025-06-12 16:02:21 +0200

    new tests from dev branch

--- test/test_singlelayer_h.jl
+++ test/test_singlelayer_h.jl
@@ -1,11 +1,11 @@
-# test that are currecntly still problematic
+# test that were problematic
 
 using CompScienceMeshes
 using GumerovKanekoDuraiswamiInts2024
 using LinearAlgebra
 using Test
 using StaticArrays
-#=
+
 vts1 = [SVector(0.001, 0.01, 0.0), SVector(0.002, 0.01, 0.0), SVector(0.001, 0.01, 0.001)]
 vts2 = [SVector(0.0, 0.001, 0.0), SVector(0.001, 0.0, 0.0), SVector(0.001, 0.001, 0.0)]
 mesh1 = Mesh(vts1, [SVector(1,2,3)])
@@ -23,11 +23,3 @@ vts2 = [SVector(0.01, 0.01, 0.0),SVector(0.01, 0.009000000000000001, 0.001),SVec
 mesh1 = Mesh(vts1, [SVector(1,2,3)])
 mesh2 = Mesh(vts2, [SVector(1,2,3)])
 @test IntegrateMesh(mesh2,mesh1) ≈ [2.7085862026852206e-12]
-
-# parallel triangles
-=#
-vts1 = [SVector(0.0, 0.01, 0.009000000000000001), SVector(0.001, 0.01, 0.009000000000000001), SVector(0.0, 0.01, 0.01)]
-vts2 = [SVector(0.009000000000000001, 0.0, 0.001), SVector(0.01, 0.0, 0.001), SVector(0.01, 0.0, 0.0)]
-mesh1 = Mesh(vts1, [SVector(1,2,3)])
-mesh2 = Mesh(vts2, [SVector(1,2,3)])
-@test IntegrateMesh(mesh2,mesh1) ≈ [1.2287605621063186e-12]
