3813cf0225e13bbcfe8b27f16ef4801d1ac58370
Leif <leifweikamp@uni-rostock.de>
2025-06-27 08:27:28 +0200

    remove broadened if statement as issue is with precision not the logic

--- src/I0.jl
+++ src/I0.jl
@@ -6,7 +6,7 @@ function I0(P, h1, h2, h3, h4)
     hh1 = hh - h1 * h1
     zero2 = zerotol * zerotol
 
-    if hh - h4^2 < zero2 * P^2 # if h1 = h2 = h3 = h4 = 0 -> Singularity # DO NOT USE h1^2 + h2^2 + h3^2
+    if hh < zero2 * P^2 # if h1 = h2 = h3 = h4 = 0 -> Singularity
         Phi1 = log(P) / P
         I = Phi1 / 6
     else
