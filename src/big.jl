70df8c8eb1e569da08e757695fd6321f48c63189
Leif <leifweikamp@uni-rostock.de>
2025-08-29 12:59:08 +0200

    playing with I6 again...

--- src/big.jl
+++ src/big.jl
@@ -3,9 +3,9 @@ using GumerovKanekoDuraiswamiInts2024
 h4 = 100.0
 h3 = 0.0
 h2 = 0.0
-#h1 = sqrt(2)/2000
-h1 = 4.9065389333867974e-18
-P1 = 0.01131370849898476
+h1 = sqrt(2)/2000
+h1 = 1e-9
+P1 = 0.01
 
 P = P1
 
@@ -13,15 +13,16 @@ I0 = GumerovKanekoDuraiswamiInts2024.I0(P1, h1, h2, h3, h4)
 
 zerotol = 3e-16
 
-
+#=
 P = BigFloat(P)
 h1 = BigFloat(h1)
 h2 = BigFloat(h2)
 h3 = BigFloat(h3)
 h4 = BigFloat(h4)
-
+=#
 
 hh = h1^2 + h2^2 + h3^2 + h4^2
+h = sqrt(h1^2 + h2^2 + h3^2 + h4^2)
 R = sqrt(P^2 + hh)
 Phi1 = 0.5 * log((P + R)^2 / hh) / P
 R1 = sqrt(P^2 + h1^2)
@@ -49,21 +50,21 @@ I = Phi1/6 - 1/2 * h4 * Phi2
 
 (h4/h1)^2 / 6 * (((3*Phi3 + h4* Phi2) - 1/(R + h4) - 3*Phi1) )
 
-@show Ref = (Phi3 - Phi1)*(Phi3 + Phi1)
+Ref = (Phi3 - Phi1)*(Phi3 + Phi1)
 Diff = P^2 * (((R1+R)^2/hh1)^2 - ((P+R)^2/hh)^2 - 2 * (R1+R)^2/hh1 + 2 * ((P+R)^2/hh))
         - h1^2 * (((P+R)^2/hh)^2 - 2 * ((P+R)^2/hh) + 1)
 
-@show Diff2= 1/4 * 1/((P^2+h1^2)*P^2)*Diff
+Diff2= 1/4 * 1/((P^2+h1^2)*P^2)*Diff
 
-@show final = (1/4 * 1/((P^2+h1^2)*P^2)*(- h1^2 * (((P+R)^2/hh)^2 - 2 * ((P+R)^2/hh) + 1)))/(Phi1+Phi3)/h1^2
+final = (1/4 * 1/((P^2+h1^2)*P^2)*(- h1^2 * (((P+R)^2/hh)^2 - 2 * ((P+R)^2/hh) + 1)))/(Phi1+Phi3)/h1^2
 
 #
 
-@show Ref = Phi3^2 - Phi1^2
-@show Alt = 1/4 * 1/((P^2+h1^2)*P^2) * (P^2 * log((R1 + R)^2 / hh1)^2 - (P^2 + h1^2)*log((P + R)^2 / hh)^2)
-@show Alt2 = 1/4 * 1/((P^2+h1^2)*P^2) * (P^2 * (log((R1 + R)^2 / hh1)^2 - log((P + R)^2 / hh)^2) - h1^2*log((P + R)^2 / hh)^2)
+Ref = Phi3^2 - Phi1^2
+Alt = 1/4 * 1/((P^2+h1^2)*P^2) * (P^2 * log((R1 + R)^2 / hh1)^2 - (P^2 + h1^2)*log((P + R)^2 / hh)^2)
+Alt2 = 1/4 * 1/((P^2+h1^2)*P^2) * (P^2 * (log((R1 + R)^2 / hh1)^2 - log((P + R)^2 / hh)^2) - h1^2*log((P + R)^2 / hh)^2)
 
-@show Alt3 = 1/4 * 1/((P^2+h1^2)*P^2) * ( 
+Alt3 = 1/4 * 1/((P^2+h1^2)*P^2) * ( 
     P^2 * (log((R1 + R)^2 / hh1) - log((P + R)^2 / hh)) * log((R1 + R)^2 / hh1) + log((P + R)^2 / hh) * (P^2 * log((R1 + R)^2 / hh1) - P^2*log((P + R)^2 / hh) - h1^2 * log((P + R)^2 / hh)) 
     )
 
@@ -132,3 +133,13 @@ In1 = Phi1/6 + 1/2 * h4^2 * (
 In2 = Phi1/6 + 1/2 * h4^2 * (
     (P^2 * log(a*b) * Ref11 - log(b)^2) / (4 * P^2 * (P^2 + h1^2)) # (Phi3^2 - Phi1^2) / h1^2
 )/(Phi1 + Phi3) - 1/6 * h4^2 * Var4 - 1/2 * h4 * Phi2
+
+
+# new method for Phi3^2 - Phi1^2
+
+cPhi = 1/2 * ((h1/h4)^2 - 1/2 * (h1/h4)^4 + 1/3 * (h1/h4)^6)
+    + 1/R * (R1 - P - 1/2 * h1^2/R + 1/3 * (R1^3 - P^3)/(P^2 + h1^2 + h4^2))
+
+I6new = Phi1/6 + 1/2 * (h4/h1)^2 * 1/(Phi1 + Phi3) * 1/(P^2 * (P^2 + h1^2)) *
+        (P^2 * (log((R1 + R)/h4) + log((P + R)/h)) * cPhi - h1^2 * log((P + R)/h)^2)
+    + 1/6*(h4/h1)^2*(h4* Phi2 - 1/(R + h4) ) - 1/2 * h4 * Phi2
